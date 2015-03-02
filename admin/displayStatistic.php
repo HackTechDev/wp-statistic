5ad46213af8b348ebee3ffce6d954035893b7339
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2015-03-02 22:14:02 +0100

    Reformat code

--- admin/displayStatistic.php
+++ admin/displayStatistic.php
@@ -1,18 +1,18 @@
 <?php
 function displayStatistic() {
-global $wpdb;
-$rows = $wpdb->get_results("
-    SELECT COUNT(*) AS nbPost, DATE_FORMAT(`post_date` , '%d/%m/%Y') AS datePost
-    FROM wp_posts
-    WHERE post_type = 'post' AND post_status = 'publish'
-    GROUP BY DATE(post_date)
-                    ");
+    global $wpdb;
+    $rows = $wpdb->get_results("
+        SELECT COUNT(*) AS nbPost, DATE_FORMAT(`post_date` , '%d/%m/%Y') AS datePost
+        FROM wp_posts
+        WHERE post_type = 'post' AND post_status = 'publish'
+        GROUP BY DATE(post_date)
+    ");
 
 ?>
 <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/css/style-admin.css" rel="stylesheet" />
 <div class="wrap">
 <h2>Post Statistic</h2>
-<table border="1">
+    <table border="1">
 <?php
 $tempYear = 0;
 $tempMonth = 0;
@@ -26,17 +26,17 @@ foreach ($rows as $row ){
     list($day, $month, $year) = explode("/", $row->datePost);
 
     if( $year != $tempYear) {
-       $line .= "<td>$year</td>"; 
+        $line .= "<td>$year</td>"; 
     } else {
-       $line .= "<td border=0></td>";
+        $line .= "<td border=0></td>";
     }
     $tempYear = $year;
 
     if( $month != $tempMonth) {
-       $line .= "<td>$month</td>"; 
-       $displaySubTotalMonth = true;
+        $line .= "<td>$month</td>"; 
+        $displaySubTotalMonth = true;
     } else {
-       $line .= "<td border=0></td>";
+        $line .= "<td border=0></td>";
     }
     $tempMonth = $month;
 
@@ -46,22 +46,22 @@ foreach ($rows as $row ){
 
     $totalPostMonth += $row->nbPost . " " ;
 
-	if($displaySubTotalMonth == true) {
-		if ($countPost != 0 ) {
-			$totalPostMonth -= $row->nbPost;	
-			echo "<td></td> <td></td> <td></td> <td></td> <td style='color:blue;'>$totalPostMonth</td>";
-			$totalPostMonth = $row->nbPost;
-		}
-		echo $line;
-		$displaySubTotalMonth = false;
-	} else {
-		echo $line;
-	}
+    if($displaySubTotalMonth == true) {
+        if ($countPost != 0 ) {
+            $totalPostMonth -= $row->nbPost;    
+            echo "<td></td> <td></td> <td></td> <td></td> <td style='color:blue;'>$totalPostMonth</td>";
+            $totalPostMonth = $row->nbPost;
+        }
+        echo $line;
+        $displaySubTotalMonth = false;
+    } else {
+        echo $line;
+    }
 
-	$countPost++;
+    $countPost++;
 }
-	 $totalPostMonth = $row->nbPost;
-	 echo "<td></td> <td></td> <td></td> <td></td> <td style='color:blue;'>$totalPostMonth</td>";
+//$totalPostMonth = $row->nbPost;
+echo "<td></td> <td></td> <td></td> <td></td> <td style='color:blue;'>$totalPostMonth</td>";
 ?>
 </table>
 </div>
